d0998b324d840390a08eac6e76a62e1adca81f18
kkwik <kevinkwik6@gmail.com>
2023-09-10 14:30:12 -0700

    feat: add reset operation

---
+++ reset_op.py
@@ -0,0 +1,38 @@
+import PySimpleGUI as sg
+from ImageOperationInterface import ImageOperationInterface
+
+
+class Reset(ImageOperationInterface):
+    @staticmethod
+    def name():
+        return 'RESET'
+
+    # Return the UI elements of the operation
+    @staticmethod
+    def get_gui():
+        contents = [
+            [
+                sg.Button('Reset ↺', key="RESET")
+            ]
+        ]
+
+        gui = sg.Column(contents)
+
+        return gui
+
+    # Operations
+    @staticmethod
+    def reset(original, modified, values):
+        return original
+
+    # Events
+    @staticmethod
+    def get_operation(operation_name):
+        if operation_name == f'{Reset.name()}':
+            return Reset.reset
+        else:
+            print("Unknown reset operation")
+        return
+
+def get_instance():
+    return Reset()
