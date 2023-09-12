7a4ffba02cd9f0714ddec3530e77292cbfd565c6
kkwik <kevinkwik6@gmail.com>
2023-09-12 12:44:32 -0700

    feat: working gray level

--- gray_level_op.py
+++ gray_level_op.py
@@ -32,13 +32,19 @@ class GrayResolution(ImageOperationInterface):
         source_gray_res = modified['gray_resolution']
         desired_gray_res = int(values['GRAY_SLIDER'])
 
-        convert = lambda val: int(f'{val:08b}'[:desired_gray_res], 2)*255 # Take val, turn it into 8-bit binary string. Get the amount of lsb we want, turn back into an int
-        convert_vect = np.vectorize(convert)
+        # Bit conversion
+        convert = lambda val: int(f'{val:08b}'[:desired_gray_res], 2) # Take val, turn it into 8-bit binary string. Get the amount of lsb we want, turn back into an int
+        convert_vect = np.vectorize(convert)                         
+        modified['image'] = convert_vect(modified['image']).astype(np.uint8) 
 
-
-        modified['image'] = convert_vect(modified['image']).astype(np.uint8)
+        # Distributing values across range 0-255 so that we can actually see the image at low bit resolution
+        modified['image'] = (255 / np.max(modified['image'])) * modified['image']   # Scale bits back to full range so we'd have 0 and 255 instead of 0 and 1
+        modified['image'] = np.floor(modified['image']).astype(np.uint8)            # Normalize values into valid pixel values
         modified['gray_resolution'] = desired_gray_res
+
         print(np.max(modified['image']))
+
+        
         return modified
 
     # Events
