ea0448528c67cf15136f2f50450867d498fe02b0
kkwik <kevinkwik6@gmail.com>
2023-10-12 09:38:49 -0700

    feat(bit plane): add hiding bit planes

--- bit_plane_op.py
+++ bit_plane_op.py
@@ -13,13 +13,16 @@ class BitPlane(ImageOperationInterface):
     def get_gui():
         contents = [
             [
-                sg.Text('Bit Plane to Show')
+                sg.Text('Bit Plane Selector')
             ],
             [
                 sg.Slider(range=(0, 7), default_value=0, size=(20,15), orientation='horizontal', key=f'{BitPlane.name()}_PLANE')
             ],
             [
-                sg.Button('Apply', key=f'{BitPlane.name()}_APPLY'),
+                sg.Button('Show', key=f'{BitPlane.name()}_SHOW_APPLY'),
+            ],
+            [
+                sg.Button('Hide', key=f'{BitPlane.name()}_HIDE_APPLY'),
             ]
         ]
 
@@ -44,11 +47,29 @@ class BitPlane(ImageOperationInterface):
         modified['image'] = result_img.astype(np.uint8) 
         return modified
 
+    @staticmethod
+    def hide_bit_plane(original, modified, window, values):
+        plane_to_hide = int(values[f'{BitPlane.name()}_PLANE'])
+
+        source_img = modified['image'] 
+        Y, X = source_img.shape # Y, X
+        result_img = np.empty(source_img.shape)
+        selected_plane = 2**plane_to_hide
+
+        for y in range(Y):
+            for x in range(X):
+                result_img[y,x] = source_img[y,x] ^ selected_plane if (source_img[y,x] & selected_plane) == selected_plane else source_img[y,x]
+
+        modified['image'] = result_img.astype(np.uint8) 
+        return modified
+
     # Events
     @staticmethod
     def get_operation(operation_name):
-        if operation_name == f'{BitPlane.name()}_APPLY':
+        if operation_name == f'{BitPlane.name()}_SHOW_APPLY':
             return BitPlane.show_bit_plane
+        elif operation_name == f'{BitPlane.name()}_HIDE_APPLY':
+            return BitPlane.hide_bit_plane
         else:
             print("Unknown BitPlane operation")
         return
